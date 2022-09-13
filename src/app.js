16e38efc575d07d18dfe75215ca576fabdc4ec9c
Murilo-MRS <murilosantana1993@gmail.com>
2022-09-13 09:06:46 -0300

    add folder para middleware e exportando

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const validateTeam = require('./middlewares/validateTeam');
+const existingId = require('./middlewares/existingId');
 
 const app = express();
 
@@ -12,31 +14,11 @@ app.use(express.json());
 
 app.get('/teams', (req, res) => res.json(teams));
 
-const existingId = (req, res, next) => {
-  const id = Number(req.params.id);
-  if (teams.some((t) => t.id === id)) {
-      next();
-  } else {
-      res.sendStatus(404);
-  }
-}
-
 app.get('/teams/:id', existingId, (req, res) => {
     const id = Number(req.params.id);
     const team = teams.find(t => t.id === id);
     res.json(team);
 });
-
-// MIDDLEWAREs post e put
-
-const validateTeam = (req, res, next) => {
-    const requiredProperties = ['nome', 'sigla'];
-    if (requiredProperties.every((property) => property in req.body)) {
-      next(); // Chama o próximo middleware
-    } else {
-      res.sendStatus(400); // Ou já responde avisando que deu errado
-    }
-  };
 
   // Arranja os middlewares para chamar validateTeam primeiro
   app.post('/teams', validateTeam, (req, res) => {
---
+++ src/middlewares/existingId.js
@@ -0,0 +1,11 @@
+
+const existingId = (req, res, next) => {
+    const id = Number(req.params.id);
+    if (teams.some((t) => t.id === id)) {
+        next();
+    } else {
+        res.sendStatus(404);
+    }
+  }
+
+  module.exports = existingId;
